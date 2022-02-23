34f984a2ef84f272cc1015a0824ca5fbad2a25f9
Abraham Gonzalez <abraham.gm08@gmail.com>
2022-02-22 22:59:12 -0800

    updated index.js

--- solutionguide.js
+++ solutionguide.js
@@ -1,6 +1,6 @@
 // map object
 const myMap = {
-	coordinates: [],
+	coordinates: [],  //Declaring properties arrays for coordinates and businesses
 	businesses: [],
 	map: {},
 	markers: {},
@@ -9,7 +9,7 @@ const myMap = {
 	buildMap() {
 		this.map = L.map('map', {
 		center: this.coordinates,
-		zoom: 11,
+		zoom: 12,
 		});
 		// add openstreetmap tiles
 		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -52,10 +52,9 @@ async function getFoursquare(business) {
 		method: 'GET',
 		headers: {
 		Accept: 'application/json',
-		Authorization: 'fsq3ATzZbmcGhdeFafr73wZcnJ+LlN6bK+4dh19a7ClS4u8='
+		Authorization: 'fsq31p4CqS6CSqCZi6PHVRf2MnYjzbU/rwJWYn5onLwGFUA='
 		}
 	}
-	let limit = 5
 	let lat = myMap.coordinates[0]
 	let lon = myMap.coordinates[1]
 	let response = await fetch(`https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
@@ -84,13 +83,21 @@ window.onload = async () => {
 	const coords = await getCoords()
 	myMap.coordinates = coords
 	myMap.buildMap()
+
+	
+
 }
 
-// business submit button
-document.getElementById('submit').addEventListener('click', async (event) => {
+
+document.getElementById("submit").addEventListener("click", async (event) => {
+
 	event.preventDefault()
 	let business = document.getElementById('business').value
 	let data = await getFoursquare(business)
 	myMap.businesses = processBusinesses(data)
 	myMap.addMarkers()
 })
+
+
+
+
